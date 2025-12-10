2ddf34ec4345ab7d3a4cbb7f7a083205ec824121
Shahid Karim <skarim759@gmail.com>
2025-12-10 18:26:30 +0545

    Add: Edit the reuired files to accept DB connection

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 from flask import Flask
 import redis
+import psycopg2
+from psycopg2 import OperationalError
+import os
 
 app = Flask(__name__)
 
 r = redis.Redis(host="redis", port=6379)
+    
+def check_db():
+     """Try connecting to PostgreSQL to see if it's running"""
+     try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST", "db"),
+            database=os.getenv("POSTGRES_DB"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            port=int(os.getenv("DB_PORT",5432))
+        )
+        conn.close()
+        return True
+     except OperationalError:
+         return False
+            
 
 @app.route("/")
-def check_redis():
+def health():
     try:
         r.ping()
-        return "Redis is UP!"
-    except:
-        return "Redis is DOWN!"
+        redis_stats = "UP"
+    except redis.exceptions.ConnectionError:
+        redis_stats = "DOWN"
+
+    db_stats = "Up" if check_db() else "DOWN!"
+    return f"Redis: {redis_stats}, Database: {db_stats}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
